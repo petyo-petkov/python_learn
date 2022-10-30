63381cb1c3a85f368b23ad3e617ed50b0561c5b5
pipon40 <115866546+pipon40@users.noreply.github.com>
2022-10-30 08:13:41 +0100

    Add files via upload

---
+++ ejer_07.py
@@ -0,0 +1,9 @@
+import ejer_07
+
+hora = time.strftime('%H')
+fin = '19'
+if hora > fin:
+    print('Hora de volver a casa')
+else:
+    q = int(fin) - int(time.strftime('%H'))
+    print('Te quedan ', q,'horas de trabajo')
