3e2602313573aed9e83399d07b9d45067b35ad9b
pipon40 <115866546+pipon40@users.noreply.github.com>
2022-10-30 08:12:34 +0100

    Delete time.py

--- time.py
+++
@@ -1,9 +0,0 @@
-import time
-
-hora = time.strftime('%H')
-fin = '19'
-if hora > fin:
-    print('Hora de volver a casa')
-else:
-    q = int(fin) - int(time.strftime('%H'))
-    print('Te quedan ', q,'horas de trabajo')
